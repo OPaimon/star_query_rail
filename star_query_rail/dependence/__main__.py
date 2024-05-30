6a98e437ac21057b4d0274861264dfcc4c710a49
OPaimon <paimoe@bupt.edu.cn>
2024-05-31 03:12:12 +0800

    :art:Format star_query_rail.dependence.__main__.py

--- star_query_rail/dependence/__main__.py
+++ star_query_rail/dependence/__main__.py
@@ -4,28 +4,29 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # 创建 SQLAlchemy 引擎
-engine = create_engine('postgresql://postgres:@localhost/starrail', echo=True)
+engine = create_engine("postgresql://postgres:@localhost/starrail", echo=True)
 
 # 创建基类
 Base = sqlalchemy.orm.declarative_base()
 
+
 class user(Base):
-    __tablename__ = 'User'
+    __tablename__ = "User"
     userid = Column(Integer, primary_key=True)
     name = Column(String)
     cookie = Column(JSON)
 
+
 class character(Base):
-    __tablename__ = 'Character'
+    __tablename__ = "Character"
     cid = Column(Integer, primary_key=True)
     name = Column(String)
 
 
-
 class connect(Base):
-    __tablename__ = 'Connect'
-    userid = Column(Integer, ForeignKey('User.userid'), primary_key=True)
-    cid = Column(Integer, ForeignKey('Character.cid'), primary_key=True)
+    __tablename__ = "Connect"
+    userid = Column(Integer, ForeignKey("User.userid"), primary_key=True)
+    cid = Column(Integer, ForeignKey("Character.cid"), primary_key=True)
     element = Column(String)
     rarity = Column(Integer)
     level = Column(Integer)
