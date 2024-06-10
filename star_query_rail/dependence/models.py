939c29903a833819a64557ca50e7f92bac80eecf
EatablePaimon <3378057121@qq.com>
2024-06-10 18:49:52 +0800

    :recycle: refactor the model.py and crud.py

--- star_query_rail/dependence/models.py
+++ star_query_rail/dependence/models.py
@@ -1,15 +1,46 @@
-from sqlmodel import JSON, Column, Field, SQLModel, create_engine
+from sqlmodel import JSON, Column, Field, SQLModel
 
 
-class Email(SQLModel, table=True):
-    email: str = Field(primary_key=True)
+class EmailBase(SQLModel):
+    email: str = Field(unique=True, index=True)
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+class EmailRegister(SQLModel):
+    email: str
     psw: str
 
 
-class Userinfo(SQLModel, table=True):
+class EmailUpdate(SQLModel):
+    psw: str
+
+
+class Email(EmailBase, table=True):
+    psw: str
+
+
+class Userbase(SQLModel):
     userid: int = Field(primary_key=True)
+    name: str = Field(default="xq")
+
+
+class UserRegister(SQLModel):
+    userid: int
     name: str
+
+
+class UserUpdate(SQLModel):
+    cookie: str
+
+
+class Userinfo(Userbase, table=True):
     cookie: dict | None = Field(default=None, sa_column=Column(JSON))
+
+
+class CharacterRegister(SQLModel):
+    cid: int
+    name: str
 
 
 class Character(SQLModel, table=True):
@@ -17,14 +48,36 @@ class Character(SQLModel, table=True):
     name: str
 
 
+class ConnectEURegister(SQLModel):
+    email: str
+    userid: int
+
+
 class ConnectEU(SQLModel, table=True):
     email: str = Field(foreign_key="email.email", primary_key=True)
     userid: int = Field(foreign_key="userinfo.userid", primary_key=True)
 
 
-class ConnectUC(SQLModel, table=True):
+class ConnectUCBase(SQLModel):
     userid: int = Field(foreign_key="userinfo.userid", primary_key=True)
     cid: int = Field(foreign_key="character.cid", primary_key=True)
+
+
+class ConnectUCRegister(SQLModel):
+    userid: int
+    cid: int
+
+
+class ConnectUCUpdate(SQLModel):
+    data: dict = Field(sa_column=Column(JSON))
+
+
+class ConnectUC(ConnectUCBase, Table=True):
+    data: dict = Field(sa_column=Column(JSON))
+
+
+'''
+class ConnectUC(ConnectUCBase, table=True):
     element: str
     rarity: int
     level: int
@@ -35,3 +88,4 @@ class ConnectUC(SQLModel, table=True):
     skills: dict = Field(sa_column=Column(JSON))
     base_type: str
     figure_path: str
+'''
